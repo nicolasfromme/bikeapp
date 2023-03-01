a99499c6fab715f390e59c7d16f4831d9972921c
nicolasfromme <nicolas1.fromme@gmail.com>
2023-03-01 23:18:39 +0100

    add auth

---
+++ src/pages/profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useUser } from '@auth0/nextjs-auth0/client';
+
+export default function Profile() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+  if (!user) return <div>Access Denied<a href="/api/auth/login">Login</a></div>;
+
+  return (
+    user && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <a href="/api/auth/logout">Logout</a>
+      </div>
+    )
+  );
+}
