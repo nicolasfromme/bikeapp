5b7528e9d9782fdd878d8f08a91c539af0106856
Joel <joho.hoffmann@gmx.de>
2023-03-20 17:19:58 +0100

    fixed graphql issue

--- src/pages/apollo-client.js
+++ src/pages/apollo-client.js
@@ -1,14 +1,37 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+// import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const corsOptions = {
-    origin: "http://localhost:3000/api/graphql",
-    credentials: true
+// const corsOptions = {
+//     origin: "http://localhost:3000/api/graphql",
+//     credentials: true
+//   };
+
+// const client = new ApolloClient({
+//     uri: "localhost:3000/api/graphql",
+//     cache: new InMemoryCache(),
+//     cors: corsOptions
+// });
+
+// export default client;
+
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: "localhost:3000/api/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: ``
+    }
   };
+});
 
 const client = new ApolloClient({
-    uri: "localhost:3000/api/graphql",
-    cache: new InMemoryCache(),
-    cors: corsOptions
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 });
 
 export default client;
