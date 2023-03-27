58db20c31adfbecfa743f00a97b907621462f390
Joel <joho.hoffmann@gmx.de>
2023-03-27 14:32:20 +0200

    added datenschutz and impressum

---
+++ app/datenschutz/page.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Container from '@mui/material/Container';
+
+
+function Datenschutz() {
+    return (
+        <Container fluid>
+            <div id="WholeText">
+                <div id="HeaderText">
+                    <h1>
+                        <center>Datenschutz</center>
+                    </h1>
+                </div>
+                <hr class="rounded" />
+                <br></br>
+                <div id="Text">
+                    <p>Die folgenden Datenschutzhinweise informieren Sie über Art und Umfang der Verarbeitung Ihrer personenbezogenen Daten im Rahmen der Nutzung unseres Onlineshops. Als gesetzliche Grundlage für den Datenschutz dient insbesondere die Datenschutz-Grundverordnung (DSGVO).</p>
+                </div>
+                <br />
+                <div>
+                    <br />
+                    <h3>Erhebung, Verarbeitung und Nutzung Ihrer Daten</h3>
+                </div>
+                <br></br>
+                <div id="Text">
+                    <p>Sie können unsere Seite besuchen, ohne Angaben zu Ihrer Person zu machen. Wir speichern lediglich Zugriffsdaten wie z. B. den Namen Ihres Internet Service Providers, die Seite, von der aus Sie uns besuchen oder den Namen der angeforderten Datei. Diese Daten werden ausschließlich zur Verbesserung unseres Angebotes ausgewertet und erlauben keinen Rückschluss auf Ihre Person.
+
+                        Personenbezogene Daten wie Vor-/Nachname, Adresse, Geb.-Datum, Telefonnummer und E-Mail-Adresse werden nur erhoben, wenn Sie uns diese im Rahmen Ihrer Warenbestellung, bei Eröffnung eines Kundenkontos oder bei Registrierung für unseren Newsletter mitteilen. Wir verwenden die von ihnen mitgeteilten Daten ohne Ihre gesonderte Einwilligung ausschließlich zur Erfüllung und Abwicklung Ihrer Bestellung und zur Zusendung unserer Kataloge/Mailings. Bei Anmeldung zum Newsletter wird Ihre E-Mail-Adresse mit Ihrer Einwilligung für eigene Werbezwecke genutzt, bis Sie sich vom Newsletter abmelden.
+
+                        Wir speichern Ihre Daten, soweit geschäftsnotwendig und im Rahmen des Bundesdatenschutzgesetzes (BDSG 2018) und der EU-Datenschutz-Grundverordnung (DSGVO) zulässig.</p>
+                </div>
+                <br />
+                <div>
+                    <br />
+                    <h3>Die Datenverarbeitungsprozesse im Einzelnen:</h3>
+                </div>
+                <br></br>
+                <div id="Text">
+                    <p>Registrierung
+                        Auf unserer Internetseite bieten wir Ihnen die Möglichkeit, sich unter Angabe personenbezogener Daten zu registrieren. Die Daten werden dabei in eine Eingabemaske eingegeben und an uns übermittelt und gespeichert. Folgende Daten werden im Rahmen des Registrierungsprozesses erhoben:
+                        Vorname, Name
+                        Geburtsdatum
+                        E-Mail-Adresse
+                        Im Zeitpunkt der Absendung der Nachricht werden zudem folgende Daten gespeichert:
+                        IP-Adresse des Nutzers
+                        Datum und Uhrzeit der Buchung
+                        Die Verarbeitung der bei der Registrierung eingegebenen Daten erfolgt zum Zwecke der Anbahnung eines Kaufvertrages (Art.6 Abs. 1 lit. b DSGVO).
+                        Die sonstigen während des Absendevorgangs verarbeiteten personenbezogenen Daten dienen dazu, einen Missbrauch des Registrierungsformulars zu verhindern und die Sicherheit unserer informationstechnischen Systeme sicherzustellen.
+                        Die Daten werden gelöscht, sobald sie für die Erreichung des Zweckes ihrer Erhebung nicht mehr erforderlich sind.
+                        Dies ist für die während des Registrierungsvorgangs erhobenen Daten der Fall, wenn die Registrierung auf unserer Internetseite aufgehoben oder abgeändert wird.
+                        Die während des Absendevorgangs zusätzlich erhobenen personenbezogenen Daten werden spätestens nach einer Frist von 30 Tagen gelöscht.
+                        Als Nutzer haben sie jederzeit die Möglichkeit, die Registrierung aufzulösen. Die über Sie gespeicherten Daten können Sie jederzeit abändern lassen.
+                        Bestellung
+                        Auf unserer Internetseite bieten wir Ihnen die Möglichkeit, unter Angabe personenbezogener Daten Waren zu bestellen. Im Rahmen des Bestellprozesses werden dabei Daten in eine Eingabemaske eingegeben und an uns übermittelt und gespeichert. Folgende Daten werden im Rahmen des Registrierungsprozesses erhoben:
+                        Vorname, Name
+                        Geburtsdatum
+                        Adresse
+                        Ggf. Telefonnummer (optional)
+                        Ggf. Bankverbindung
+                        E-Mail-Adresse
+                        Im Zeitpunkt der Absendung der Nachricht werden zudem folgende Daten gespeichert:
+                        IP-Adresse des Nutzers
+                        Datum und Uhrzeit der Buchung
+                        Die Verarbeitung der bei der Bestellung eingegebenen Daten erfolgt zum Zwecke der Begründung und Durchführung eines Kaufvertrages (Art.6 Abs. 1 lit. b DSGVO).
+                        Die sonstigen während des Absendevorgangs verarbeiteten personenbezogenen Daten dienen dazu, einen Missbrauch des Bestellformulars zu verhindern und die Sicherheit unserer informationstechnischen Systeme sicherzustellen.
+                        Die Daten werden gelöscht, sobald sie für die Erreichung des Zweckes ihrer Erhebung nicht mehr erforderlich sind.
+                        Nach der Erfüllung des Kaufvertrages werden Ihre Daten gelöscht, es sei denn wir sind aufgrund handels- und/oder steuerrechtlicher Vorschriften verpflichtet, die Daten darüber hinaus zu speichern.
+                        Die während des Absendevorgangs zusätzlich erhobenen personenbezogenen Daten werden spätestens nach einer Frist von 30 Tagen gelöscht.</p>
+                </div>
+            </div>
+        </Container>
+    );
+}
+
+export default Datenschutz;
