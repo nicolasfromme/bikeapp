0106c86042fc298ac3fe3c6a77e57f9a216d9fb5
DEV-AdrianC <catinella.adrian@gmail.com>
2023-04-11 21:40:36 +0200

    userprofile_overview_page

---
+++ src/pages/profileoverview.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Container from '@mui/material/Container';
+
+
+
+
+function profileoverview() {
+    return (
+      <Container fluid>
+        <div id="WholeText">
+          <div id="HeaderText">
+            <h1>
+              <center>Kontoübersicht</center>
+            </h1>
+          </div>
+          
+        </div>
+      </Container>
+    );
+  }
+  
+  export default profileoverview;
